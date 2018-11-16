3f72207dbf263eb584d9e8249853bf1990032d85
jagannath808 <45080515+jagannath808@users.noreply.github.com>
2018-11-16 11:12:17 +0530

    Add files via upload

---
+++ Poll_Win_Count.java
@@ -0,0 +1,64 @@
+package com.Collection;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+
+public class Poll_Win_Count {
+
+	public static void main(String[] args) {
+		
+		List<String> list = new ArrayList<String>();
+		list.add("Jessy");
+		list.add("Philip");
+		list.add("Jessy");
+		list.add("Philip");
+	
+		
+		
+//		Iterator itr= list.iterator();
+//		while(itr.hasNext()){
+//			System.out.println(itr.next());
+//		}
+		for(String str:list){
+			
+		}
+		
+		
+			Map<String,Integer> map= new HashMap<String,Integer>();
+
+			for(String str1:list){
+				
+				if (map.containsKey(str1)) {
+		            map.put(str1, map.get(str1) + 1);
+		         } else {
+		            map.put(str1, 1);
+		         }
+			}
+	int max = 0; 
+	        String winner = ""; 
+	        for (Map.Entry<String,Integer> entry : map.entrySet()) 
+	        { 
+	            String key  = entry.getKey(); 
+	       	            Integer val = entry.getValue(); 
+	        
+	            if (val > max) 
+	            { 
+	                max = val; 
+	                winner = key; 
+	            } 
+	            else if (val == max && 
+	                    winner.compareTo(key) > 0) 
+	                    winner = key; 
+	            } 
+	            System.out.println(winner); 
+	        }
+	        
+			
+	}
+
+
---
+++ SortArray.java
@@ -0,0 +1,70 @@
+package com.Collection;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class SortArray {
+
+	public static void main(String[] args) {
+		
+		int[] arr={4, 5, 7, 7, 6, 5, 4, 3};
+	
+		List<Integer> lst =new ArrayList<Integer>();
+		for(Integer i:arr){
+			lst.add(i);	
+		}
+		
+	Collections.sort(lst);	
+		 
+		Map<Integer, Integer> map=new HashMap<Integer,Integer>();
+		for(Integer i: lst){
+			if(map.containsKey(i)){
+				map.put(i, map.get(i)+1);
+			}
+			else
+			{
+			map.put(i,1);
+			}
+		}
+	
+		int  ky=0;
+		int value=0;
+		Set<Entry<Integer, Integer>> st= map.entrySet();
+		
+		for(Map.Entry<Integer, Integer> en:st){
+			if(en.getValue()<2){
+		System.out.print(en.getKey());
+		System.out.print(" ");
+		
+		}
+			
+		}
+		 int min=1;int k=0;int j=0;
+		do{
+			min++;
+		 for (Map.Entry<Integer, Integer> entry : map.entrySet()){
+			value=entry.getValue();
+			if(entry.getValue()==min){
+				ky=entry.getKey();
+				for(int i=0;i<min;i++){
+					System.out.print(ky);
+					System.out.print(" ");
+				}
+				
+			}
+			
+		}
+			
+		}while(j<=st.size());	
+	}
+
+}
